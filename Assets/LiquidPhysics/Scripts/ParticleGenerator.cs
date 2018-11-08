d61b8cb8fcc6c993e1af902f281d79ee5002095b
shshwdr <cs.vivian04@gmail.com>
2018-11-07 23:49:14 -0800

    some trys on making fluid

--- Assets/LiquidPhysics/Scripts/ParticleGenerator.cs
+++ Assets/LiquidPhysics/Scripts/ParticleGenerator.cs
@@ -12,7 +12,7 @@ using System.Collections;
 /// </summary>
 
 public class ParticleGenerator : MonoBehaviour {		
-	float SPAWN_INTERVAL=0.025f; // How much time until the next particle spawns
+	public float SPAWN_INTERVAL=0.025f; // How much time until the next particle spawns
 	float lastSpawnTime=float.MinValue; //The last spawn time
 	public int PARTICLE_LIFETIME=3; //How much time will each particle live
 	public Vector3 particleForce; //Is there a initial force particles should have?
